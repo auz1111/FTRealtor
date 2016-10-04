df16e6ab808509271d507dd3869daa3892995028
auz11 <auz11@AUZOPTIMUS>
2016-10-03 19:47:08 -0700

    Add project files.

---
+++ FTRealtor/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FTRealtor.Startup))]
+namespace FTRealtor
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
